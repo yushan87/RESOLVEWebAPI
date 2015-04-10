e68234de27c0fb17bb80cf03960a44e9a6fac401
Yu-Shan Sun <yushans@g.clemson.edu>
2015-04-10 17:28:13 -0400

    Created a CompilerSocket class

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,19 +2,9 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import play.mvc.WebSocket;
 import views.html.index;
 
 public class Application extends Controller {
-
-    public static WebSocket<String> socket() {
-        return WebSocket.whenReady((in, out) -> {
-            // Send a single 'Hello <Name>!' message
-            // and close the connection.
-            out.write("Hello!");
-            out.close();
-        });
-    }
 
     public static Result index() {
         return ok(index.render("The RESOLVE Web API is currently online."));
