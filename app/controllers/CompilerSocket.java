cbe43b84d38abd8fab2ce6dc1e64dcd6d735ff3f
Yu-Shan Sun <yushans@g.clemson.edu>
2015-04-14 13:52:16 -0400

    Depending on the parameters passed, it redirects to the right web socket.

--- app/controllers/CompilerSocket.java
+++ app/controllers/CompilerSocket.java
@@ -1,13 +1,30 @@
 package controllers;
 
-import models.actors.GeneralSocketActor;
+import models.actors.*;
 import play.mvc.Controller;
 import play.mvc.WebSocket;
 
 public class CompilerSocket extends Controller {
 
-    public static WebSocket<String> socket() {
-        return WebSocket.withActor(GeneralSocketActor::props);
+    public static WebSocket<String> socket(String job, String project) {
+        if (job == null || project == null) {
+            return WebSocket.withActor(GeneralSocketActor::props);
+        }
+        else if (job.equals("buildJar")) {
+            return WebSocket.withActor(JarSocketActor::props);
+        }
+        else if (job.equals("ccverify")) {
+            return WebSocket.withActor(CCVerifySocketActor::props);
+        }
+        else if (job.equals("genVCs")) {
+            return WebSocket.withActor(VCSocketActor::props);
+        }
+        else if (job.equals("rwverify")) {
+            return WebSocket.withActor(RWVerifySocketActor::props);
+        }
+        else {
+            return WebSocket.withActor(TranslateSocketActor::props);
+        }
     }
 
 }
