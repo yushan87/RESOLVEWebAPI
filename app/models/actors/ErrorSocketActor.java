8739e521b560576e04dc7c420cec9a95bfcfc3f9
Yu-Shan Sun <yushans@g.clemson.edu>
2015-05-06 09:41:43 -0400

    small change

--- app/models/actors/ErrorSocketActor.java
+++ app/models/actors/ErrorSocketActor.java
@@ -3,14 +3,21 @@ package models.actors;
 import akka.actor.ActorRef;
 import akka.actor.PoisonPill;
 import akka.actor.Props;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
 
 public class ErrorSocketActor extends AbstractSocketActor {
 
     public ErrorSocketActor(ActorRef out) {
         super(out, "errorhandler");
 
+        // Create the error JSON Object
+        ObjectNode result = Json.newObject();
+        result.put("status", "error");
+        result.put("msg", "Undefined job request");
+
         // Send the message through the websocket
-        myWebSocketOut.tell("Undefined job request", self());
+        myWebSocketOut.tell(result.toString(), self());
 
         // Close the connection
         self().tell(PoisonPill.getInstance(), self());
