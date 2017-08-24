5671a94079ecabbe42909b74e650a68809ae614d
Yu-Shan Sun <yushans@g.clemson.edu>
2017-08-24 13:11:29 -0400

    Assynchronous part didn't work

--- app/controllers/WebSocketController.java
+++ app/controllers/WebSocketController.java
@@ -20,8 +20,6 @@ import compiler.actors.invokers.*;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import play.libs.streams.ActorFlow;
@@ -93,7 +91,7 @@ public class WebSocketController extends Controller {
      *
      * @return A {@link WebSocket} object.
      */
-    public final CompletionStage<WebSocket> socket(String job, String project) {
+    public final WebSocket socket(String job, String project) {
         WebSocket socket;
 
         // Check to see if that project folder exists
@@ -146,7 +144,7 @@ public class WebSocketController extends Controller {
                             myActorSystem, myStreamMaterializer));
         }
 
-        return CompletableFuture.completedFuture(socket);
+        return socket;
     }
 
     // ===========================================================
