53e158841495b5a70a27f454a66df47058d3a08b
Yu-Shan Sun <yushans@g.clemson.edu>
2017-12-15 11:19:12 -0500

    Fixes problems with the JUnit test

--- test/routing/RouterTest.java
+++ test/routing/RouterTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * ---------------------------------
  * Copyright (c) 2017
  * RESOLVE Software Research Group
@@ -9,12 +9,18 @@
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
  */
+
 package routing;
 
+import java.io.File;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import play.Application;
+import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Http.RequestBuilder;
 import play.mvc.Result;
-import play.test.WithApplication;
+import play.test.Helpers;
 
 import static org.junit.Assert.assertEquals;
 import static play.mvc.Http.Status.NOT_FOUND;
@@ -22,23 +28,54 @@ import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.route;
 
 /**
- * <p>The JUnit Test is designed to test all routes provided by
- * the RESOLVE WebAPI.</p>
+ * <p>This {@code JUnit} Test is designed to test all routes
+ * provided by the {@code RESOLVE WebAPI}.</p>
  *
  * @author Yu-Shan Sun
  * @version 1.0
  */
-public class RouterTest extends WithApplication {
+public class RouterTest {
+
+    // ===========================================================
+    // Member Fields
+    // ===========================================================
+
+    /** <p>A fake instantiation of this API app.</p> */
+    private Application myApplication;
+
+    // ===========================================================
+    // Public Methods
+    // ===========================================================
+
+    /**
+     * <p>Setup for each route test.</p>
+     */
+    @Before
+    public final void setup() {
+        myApplication =
+                new GuiceApplicationBuilder().configure("webapi.workingdir",
+                        new File(".").getAbsolutePath()).build();
+        Helpers.start(myApplication);
+    }
+
+    /**
+     * <p>Tear down code for each route test.</p>
+     */
+    @After
+    public final void tearDown() {
+        Helpers.stop(myApplication);
+        myApplication = null;
+    }
 
     /**
      * <p>A simple test to ensure we get a 404.</p>
      */
     @Test
-    public void testBadRoute() {
+    public final void testBadRoute() {
         RequestBuilder request =
-                new RequestBuilder().method("GET").uri("/xx/Kiwi");
+                Helpers.fakeRequest().method("GET").uri("/xx/Kiwi");
 
-        Result result = route(request);
+        Result result = route(myApplication, request);
         assertEquals(NOT_FOUND, result.status());
     }
 
@@ -46,10 +83,10 @@ public class RouterTest extends WithApplication {
      * <p>Check to see if we can retrieve the main index page.</p>
      */
     @Test
-    public void testIndexRoute() {
-        RequestBuilder request = new RequestBuilder().method("GET").uri("/");
+    public final void testIndexRoute() {
+        RequestBuilder request = Helpers.fakeRequest().method("GET").uri("/");
 
-        Result result = route(request);
+        Result result = route(myApplication, request);
         assertEquals(OK, result.status());
     }
 
